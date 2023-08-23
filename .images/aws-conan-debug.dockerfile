bc989ab92af6d49721b855518f6a4099d794ff7f
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2023-08-23 04:32:43 +0200

    Add processing files from Include and Source folder for conan scripts and update locations using debug and release configurations

---
+++ .images/aws-conan-debug.dockerfile
@@ -0,0 +1,18 @@
+FROM mikhail.shostak/python
+
+RUN apt-get install -y cmake
+RUN pip install conan
+
+RUN conan profile detect
+
+#RUN apt-get install -y sudo
+#RUN useradd -ms /bin/bash wsl && echo "wsl:wsl" | chpasswd && adduser wsl sudo
+
+COPY .scripts/conan /work/.conan
+
+ENV PATH=$PATH:/work:/work/.conan
+
+WORKDIR /work
+
+#USER wsl
+CMD conan-install-debug ; sleep infinity
--- .scripts/conan/conanfile.py
+++ .scripts/conan/conanfile.py
@@ -1,12 +1,13 @@
 from conan import ConanFile, tools
 from conan.tools.cmake import CMake
 
-import os, yaml, glob
+import os, yaml, glob, hashlib
 
 class ProjectGenerator(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
+    project_base_dir = 'project'
     project_path = None
     project_name = None
     _project = None
@@ -16,7 +17,7 @@ class ProjectGenerator(ConanFile):
         if self._project:
             return self._project
 
-        project_files = glob.glob('*.project')
+        project_files = glob.glob(f'{self.project_base_dir}/*.project')
 
         self.project_path = os.path.join(os.getcwd(), project_files[0])
         self.project_name = os.path.splitext(os.path.basename(self.project_path))[0]
@@ -27,6 +28,20 @@ class ProjectGenerator(ConanFile):
             self._project = yaml.load(f, Loader=yaml.FullLoader)
 
         return self._project
+
+    def get_source_paths(self, project_base_dir):
+        return glob.glob(f'{self.project_base_dir}/{project_base_dir}/**/*.cpp', recursive=True)
+
+    def get_all_subfolder_paths(self, project_base_dir):
+        return [name for name in glob.glob(f'{self.project_base_dir}/{project_base_dir}/**/*/', recursive=True) if os.path.isdir(name)]
+
+    def save_file(self, path, content):
+        content_hash = hashlib.sha256(content.encode()).hexdigest()
+        file_hash = hashlib.sha256(open(path, 'rb').read()).hexdigest() if os.path.isfile(path) else None
+
+        print(f'{path}: {file_hash} -> {content_hash}')
+        if file_hash != content_hash:
+            tools.files.save(self, path, content)
 
     def requirements(self):
         for dependency in self.project['Dependencies']:
@@ -43,7 +58,6 @@ class ProjectGenerator(ConanFile):
             package = dependency.split('/')[0]
             cmake_find_packages.append(f'find_package({package} REQUIRED)')
             cmake_link_packages.append(f'{package}::{package}')
-            cmake_packages.append(f'{package}\n')
 
         version = self.project['Version']
 
@@ -57,7 +71,20 @@ class ProjectGenerator(ConanFile):
         if cmake_find_packages:
             cmake_content.extend(cmake_find_packages)
 
-        cmake_content.append(f'add_executable({self.project_name} /opt/source/Source/main.cpp)')
+        source_base_dir = 'Source'
+        include_base_dir = 'Include'
+
+        source_cpp_paths = self.get_source_paths(source_base_dir)
+        include_subfolders = self.get_all_subfolder_paths(include_base_dir)
+        source_subfolders = self.get_all_subfolder_paths(source_base_dir)
+
+        print(source_cpp_paths)
+        print(include_subfolders)
+        print(source_subfolders)
+
+        cmake_sources = ' '.join(source_cpp_paths)
+
+        cmake_content.append(f'add_executable({self.project_name} {cmake_sources})')
         if cmake_link_packages:
             packages = ' '.join(cmake_link_packages)
             cmake_content.append(f'target_link_libraries({self.project_name} {packages})')
@@ -66,7 +93,7 @@ class ProjectGenerator(ConanFile):
 
         print(cmake_content)
 
-        tools.files.save(self, cmake_project_path, cmake_content)
+        self.save_file(cmake_project_path, cmake_content)
 
         cmake = CMake(self)
         cmake.configure(build_script_folder=self.folders.build_folder)
